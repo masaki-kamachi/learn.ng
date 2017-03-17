08fd19af7cf5b01a691aab4c0b33eec02c7d99a9
masaki-kamachi <masaki.kamachi@bizreach.co.jp>
2017-03-17 20:01:36 +0900

    first day

--- src/components/number.button/number.button.component.ts
+++ src/components/number.button/number.button.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'calc-num-button',
   templateUrl: '../abstract.button/abstract.button.component.html',
   styleUrls: ['../abstract.button/abstract.button.component.scss']
 })
-export class NumberButtonComponent {}
+export class NumberButtonComponent {
+  @Input() public value: number;
+  @Input() public total: number;
+
+  get text () : string {
+    return String(this.value);
+  }
+
+  public onClick(): void {
+    this.total += this.value;
+    console.log(this.total);
+  }
+
+  public ngOnInit(): void {
+    console.log(this.value);
+  }
+}
